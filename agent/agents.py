6c43c2f1b52ec87735339d113213aef80dba6452
Udhay707 <udhaychandhar@gmail.com>
2025-04-09 10:01:12 +0100

    update latency clarification agent

--- agent/agents.py
+++ agent/agents.py
@@ -32,7 +32,7 @@ def router(state: GraphState):
     log.info("The saved varibles are fetched for the user %s", saved_variables)
     if saved_variables[VariableStore.LATENCY.name]:
         return Agents.ClarificationSubgraph.value
-    return Agents.ClarificationSubgraph.value
+    return END
         
 
 graph.set_entry_point(Agents.LatencyAnayserAgent.value)
